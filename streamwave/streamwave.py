9c8224dd86f84ed17a10d462baf5f0b5b5b5468a
Rob McAuley <rmcauley@gmail.com>
2020-12-27 16:47:08 +0100

    Fix connect/disconnect logic

--- streamwave/streamwave.py
+++ streamwave/streamwave.py
@@ -20,7 +20,7 @@ class Streamwave(discord.Client):
     log.debug(f"Streaming to {self.settings.audio_channel}")
     source = self.settings.audio_source
     vc = await channel.connect()
-    # restream the Opus stream to Discord, avoiding re-encoding
+    # use from_probe to avoid re-encoding the stream on its way to discord
     audio_source = await discord.FFmpegOpusAudio.from_probe(source)
     vc.play(audio_source)
 
@@ -40,23 +40,40 @@ class Streamwave(discord.Client):
   async def on_ready(self) -> None:
     # check to see if anyone's listening after we've started
     channel = self.get_channel(self.settings.audio_channel)
-    if len(channel.members) > 0:
+    if len(channel.voice_states) > 0:
       log.debug(f"Start-up check: Listeners waiting on {self.settings.audio_channel}")
       await self.streamwave_start(channel)
     else:
       log.debug(f"Start-up check: Nobody waiting on {self.settings.audio_channel}")
 
   async def on_voice_state_update(self, member, before, after) -> None:
-    channel = after.channel or before.channel
-    if not channel or channel.id != self.settings.audio_channel:
+    channel = None
+
+    # only look at channel if state has been changed for us
+    if str(after) != str(before):
+      # after.channel will be None if disconnecting, populated if switching or connecting
+      if after.channel is not None and after.channel.id == self.settings.audio_channel:
+        channel = after.channel
+      # before.channel will be None if connecting for first time, populated if they are coming from a different channel
+      elif before.channel is not None and before.channel.id == self.settings.audio_channel:
+        channel = before.channel
+
+    if not channel:
       return
 
-    # will represent the list of channels currently connected to
-    voicelist = [v.channel.id for v in self.voice_clients]
+    # Filter out ourselves from the member list, and anyone else's voice status that's from another channel
+    listeners = [
+      member_id
+      for member_id, voice_state
+      in channel.voice_states.items()
+      if member_id != self.user.id
+      and voice_state.channel
+      and voice_state.channel.id == self.settings.audio_channel
+    ]
 
     # if we're the only ones left, disconnect
-    if len(channel.members) == 1 and channel.members[0].id == self.user.id:
+    if len(listeners) == 0:
       await self.streamwave_stop(channel)
-    # if we haven't connected yet, start
-    elif len(channel.members) > 0 and channel.id not in voicelist:
+    # if we don't have this channel ID in our voice client list, connect
+    elif not next((v.channel.id for v in self.voice_clients), None):
       await self.streamwave_start(channel)
