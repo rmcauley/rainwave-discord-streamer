78171e4b13bef31ee9e233df38ecd8f04ac15f12
Rob McAuley <rmcauley@gmail.com>
2021-05-05 23:46:48 +0200

    Bring back activity-streamer, update now playing format

--- streamwave/now_playing.py
+++ streamwave/now_playing.py
@@ -9,6 +9,19 @@ class NowPlaying:
     def __init__(self, sid):
         self.sid = sid
 
+    def format_song(self, rw_event):
+        result = ""
+        if rw_event["type"] == "OneUpProducer":
+            result += "\U0001F31FPOWER HOUR\U000025B6 "
+        elif rw_event["type"] == "PVPElectionProducer":
+            result += "\U0001F94APVP\U000025B6 "
+
+        song = rw_event["songs"][0]
+        result += song["albums"][0]["name"]
+        result += ": " + song["title"]
+        result += " (" + ", ".join(artist["name"] for artist in song["artists"]) + ")"
+        return result
+
     # Function to be run in its own thread so that each bot can update its own status to the currently playing song, album, and artist
     async def start(self, client):
         first = True
@@ -19,22 +32,9 @@ class NowPlaying:
             # Current song data is the first song in the info API call
             with request.urlopen("http://rainwave.cc/api4/info?sid=" + sid) as url:
                 data = json.loads(url.read().decode())
-                artists = data["sched_current"]["songs"][0]["artists"]
-                for a in artists:
-                    alist += a["name"] + ", "
-                alist = alist[: len(alist) - 2]
-                song = data["sched_current"]["songs"][0]["title"]
-                alb = data["sched_current"]["songs"][0]["albums"][0]["name"]
-                # set status to the current song and artist
-                # print(song)
                 now_play = Activity(
                     type=ActivityType.listening,
-                    name="\U0000266B "
-                    + song
-                    + "  \U0001F4D6 "
-                    + alb
-                    + "  \U0001F9B2 "
-                    + alist,
+                    name=self.format_song(data["sched_current"]),
                 )
                 if client.ws:
                     await client.change_presence(activity=now_play)
--- streamwave/streamwave.py
+++ streamwave/streamwave.py
@@ -1,16 +1,12 @@
-import os
-import discord
 import asyncio
-import threading
 import logging
-import urllib
-import json
-from time import time
-from discord import Activity, ActivityType
-from urllib import request
 from urllib.parse import urlparse
-from discord.ext import commands
+
+import discord
+
 from .settings_class import StationSettings
+
+log = logging.getLogger("streamwave")
 
 
 class Streamwave(discord.Client):
@@ -21,17 +17,77 @@ class Streamwave(discord.Client):
         super().__init__(*args, **kwargs)
         self.settings = settings
 
+    async def streamwave_start(self, channel) -> None:
+        log.debug(f"Streaming to {self.settings.audio_channel}")
+        source = self.settings.audio_source
+        vc = await channel.connect()
+        # use from_probe to avoid re-encoding the stream on its way to discord
+        self.audio_source = await discord.FFmpegOpusAudio.from_probe(source)
+        vc.play(self.audio_source)
+
+    async def streamwave_stop(self, channel) -> None:
+        for v in self.voice_clients:
+            if v.channel.id == channel.id:
+                log.debug(f"Stopping streaming to {self.settings.audio_channel}")
+                v.stop()
+                self.audio_source.cleanup()
+                await v.disconnect()
+
     async def logout(self) -> None:
         for v in self.voice_clients:
             v.stop()
             await v.disconnect()
         await super().logout()
 
-    # establish background task, join the channel, and play the music
     async def on_ready(self) -> None:
+        # check to see if anyone's listening after we've started
         channel = self.get_channel(self.settings.audio_channel)
-        source = self.settings.audio_source
-        vc = await channel.connect()
-        # use from_probe to avoid re-encoding the stream on its way to discord
-        self.audio_source = await discord.FFmpegOpusAudio.from_probe(source)
-        vc.play(self.audio_source)
+        if len(channel.voice_states) > 0:
+            log.debug(
+                f"Start-up check: Listeners waiting on {self.settings.audio_channel}"
+            )
+            await self.streamwave_start(channel)
+        else:
+            log.debug(
+                f"Start-up check: Nobody waiting on {self.settings.audio_channel}"
+            )
+
+    async def on_voice_state_update(self, member, before, after) -> None:
+        channel = None
+
+        # on_voice_state_update will fire every time the voice state of the server changes.
+        # this includes people being muted in the same channel.
+        # so doing str(after) != str(before) filters it down to just when people have changed channels
+        # this if statement works because getting the string of the after and before states returns just the name of the channels.
+        if str(after) != str(before):
+            # after.channel will be None if someone is disconnecting, populated if switching or connecting
+            if (
+                after.channel is not None
+                and after.channel.id == self.settings.audio_channel
+            ):
+                channel = after.channel
+            # before.channel will be None if someone is connecting for first time, populated if they are coming from a different channel
+            elif (
+                before.channel is not None
+                and before.channel.id == self.settings.audio_channel
+            ):
+                channel = before.channel
+
+        if not channel:
+            return
+
+        # Filter out ourselves from the member list, and anyone else's voice status that's from another channel
+        listeners = [
+            member_id
+            for member_id, voice_state in channel.voice_states.items()
+            if member_id != self.user.id
+            and voice_state.channel
+            and voice_state.channel.id == self.settings.audio_channel
+        ]
+
+        # if we're the only ones left, disconnect
+        if len(listeners) == 0:
+            await self.streamwave_stop(channel)
+        # if we don't have this channel ID in our voice client list, connect
+        elif not next((v.channel.id for v in self.voice_clients), None):
+            await self.streamwave_start(channel)
